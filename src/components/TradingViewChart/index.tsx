852ab10fdf05a1ca051115abe473ce71c510bcee
Lucas Low <>
2024-03-14 17:19:39 +0800

    disable tradingview features select symbol

--- src/components/TradingViewChart/index.tsx
+++ src/components/TradingViewChart/index.tsx
@@ -23,10 +23,34 @@ export default function TradingViewChart({ ohlcData }: Props) {
                     theme: "dark",
                     style: "1",
                     locale: "en",
-                    toolbar_bg: "#f1f3f6",
+                    toolbar_bg: "#1C1C1C",
                     enable_publishing: false,
-                    allow_symbol_change: true,
+                    allow_symbol_change: false,
                     container_id: chartContainerRef.current?.id || "",
+                    drawings_access: {
+                        type: "black",
+                        tools: [{ name: "Regression Trend" }],
+                    },
+                    disabled_features: [
+                        "header_symbol_search",
+                        "header_screenshot",
+                        "header_compare",
+                        "display_market_status",
+                        "study_templates",
+                        "left_toolbar",
+                        "control_bar",
+                        "main_series_scale_menu",
+                        "symbol_info",
+                        "show_hide_button_in_legend",
+                        "pane_context_menu",
+                        "main_series_scale_menu",
+                    ],
+                    overrides: {
+                        "paneProperties.background": "#1C1C1C",
+                        "paneProperties.vertGridProperties.color": "#232323",
+                        "paneProperties.horzGridProperties.color": "#232323",
+                        "scalesProperties.textColor": "#E0E0E0",
+                    },
                 });
             }
         };
